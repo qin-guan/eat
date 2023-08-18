e731fba6dc7637086ad62babd4fdb633e474d0be
Qin Guan <qinguan20040914@gmail.com>
2023-08-18 21:19:29 +0800

    wip: quick debug
    i'm eating at mcdonald's

--- plugins/seed.ts
+++ plugins/seed.ts
@@ -2,7 +2,9 @@ import { APP_DATA_KEY, type AppData } from '../utils/data'
 
 export default defineNitroPlugin(async () => {
   const storage = useStorage<AppData>('eat')
-  if (!(await storage.getItem(APP_DATA_KEY))) {
+  const data = await storage.getItem(APP_DATA_KEY)
+  console.log(data)
+  if (!data) {
     await storage.setItem(APP_DATA_KEY, DEFAULT_APP_DATA)
   }
 })
