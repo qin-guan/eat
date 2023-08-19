671f153b7836ead818bea580a279118b5d7ba342
Qin Guan <qinguan20040914@gmail.com>
2023-08-19 17:01:01 +0800

    feat: funny

--- routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { AppData } from "../utils/data"
-
-export default eventHandler(async () => {
-  const storage= useStorage<AppData>('eat')
-  const data = await storage.getItem(APP_DATA_KEY)
-  console.log('cdata2', data)
-  return data
-})
---
+++ routes/v1/locations/index.post.ts
@@ -0,0 +1,25 @@
+import { AppData } from "../../../utils/data"
+
+export default defineEventHandler(async (event) => {
+  const storage = useStorage<AppData>('eat')
+  const data = await storage.getItem(APP_DATA_KEY)
+  const body = await readBody(event)
+
+  if (!body.name || !body.totalCapacity || !body.currentCapacity) {
+    return createError({
+      status: 400,
+    })
+  }
+
+  data.locations.push({
+    id: data.locations.length + 1,
+    name: body.name,
+    totalCapacity: body.totalCapacity,
+    currentCapacity: body.currentCapacity,
+  });
+  data.lastUpdated = Date.now();
+
+  await storage.setItem(APP_DATA_KEY, data)
+
+  setResponseStatus(event, 201)
+})
