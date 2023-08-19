a5a31d46c3561fb0b317f17d5e58a1828da91e40
Qin Guan <qinguan20040914@gmail.com>
2023-08-19 11:36:18 +0800

    wip: debug

--- plugins/seed.ts
+++ plugins/seed.ts
@@ -1,6 +1,7 @@
 import { APP_DATA_KEY, type AppData } from '../utils/data'
 
 export default defineNitroPlugin(async () => {
+  console.log('plugin is running!')
   const storage = useStorage<AppData>('eat')
   const data = await storage.getItem(APP_DATA_KEY)
   console.log('cdata', data)
